d819e9ee5e8b5922103308ba6b2a5e01bc16d714
lafeingcrokodil <tvhtoo@gmail.com>
2019-12-29 22:02:13 +0100

    cosmetic changes to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,47 +2,43 @@ new Vue({
   el: '#app',
 
   data: {
-    ws: null, // Our websocket
-    newMsg: '', // Holds new messages to be sent to the server
-    chatContent: '', // A running list of chat messages displayed on the screen
-    username: null, // Our username
-    joined: false // True if username has been filled in
+    ws: null, // our websocket
+    newMsg: '', // new message to be sent to the server
+    chatContent: '', // running list of chat messages displayed on the screen
+    username: null, // our username
+    joined: false // true if username has been filled in
   },
 
   created: function() {
-    var self = this;
+    let self = this;
     this.ws = new WebSocket('ws://' + window.location.host + '/ws');
     this.ws.addEventListener('message', function(e) {
-      var msg = JSON.parse(e.data);
-      self.chatContent += '<div class="chip">'
-        + msg.username
-        + '</div>'
-        + msg.message + '<br/>';
-
-      var element = document.getElementById('chat-messages');
-      element.scrollTop = element.scrollHeight; // Auto scroll to the bottom
+      let msg = JSON.parse(e.data);
+      self.chatContent += `<div class="chip">${msg.username}</div>${msg.message}<br/>`;
+      let element = document.getElementById('chat-messages');
+      element.scrollTop = element.scrollHeight; // auto scroll to the bottom
     });
   },
 
   methods: {
     send: function () {
-      if (this.newMsg != '') {
+      if (this.newMsg) {
         this.ws.send(
           JSON.stringify({
             username: this.username,
-            message: $('<p>').html(this.newMsg).text() // Strip out html
+            message: $('<p>').html(this.newMsg).text() // strip out html
           })
         );
-        this.newMsg = ''; // Reset newMsg
+        this.newMsg = ''; // reset newMsg
       }
     },
 
     join: function () {
       if (!this.username) {
-        M.toast({html: 'You must choose a username', displayLength: 2000});
+        M.toast({html: 'Please choose a username.', displayLength: 2000});
         return;
       }
-      this.username = $('<p>').html(this.username).text();
+      this.username = $('<p>').html(this.username).text(); // strip out html
       this.joined = true;
     }
   }
