5984074420afd088cc84287b08a14cac846ea7e4
lafeingcrokodil <tvhtoo@gmail.com>
2019-12-31 21:56:59 +0100

    now with websockets

--- public/scripts/multirogue.js
+++ public/scripts/multirogue.js
@@ -13,13 +13,31 @@ let MultiRogue = new Vue({
       }
       this.rows.push(row);
     }
+    
+    this.ws = new WebSocket('ws://' + window.location.host + '/ws');
+    this.ws.addEventListener('message', this.handleEvent.bind(this));
   },
 
   methods: {
-    display: function(c, x, y) {
+    display: function({c, x, y}) {
       let row = this.rows[y]; // in the yth row...
       row.splice(x, 1, c); // replace the xth character with c
       Vue.set(this.rows, y, row);
+    },
+
+    handleEvent: function(e) {
+      let event = JSON.parse(e.data);
+      switch (event.name) {
+        case 'display':
+          this.display(JSON.parse(event.data));
+      }
+    },
+
+    fireEvent: function(name, data) {
+      this.ws.send(JSON.stringify({
+        name: name,
+        data: JSON.stringify(data)
+      }));
     }
   }
 });
--- server/server.go
+++ server/server.go
@@ -4,17 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 // Server is a MultiRogue server.
 type Server struct {
-	addr string
+	addr     string                   // TCP address to listen on
+	players  map[*websocket.Conn]bool // players that are currently connected
+	upgrader websocket.Upgrader       // for upgrading HTTP requests to websockets
+}
+
+// Event is something that happens in the game, like a player moving.
+type Event struct {
+	// Name is the name of the event, e.g. "move".
+	Name string `json:"name"`
+	// Data contains additional information like the direction of movement.
+	Data string `json:"data"`
 }
 
 // New returns a new Server instance.
 func New(port int) *Server {
 	return &Server{
-		addr: fmt.Sprintf(":%d", port), // TCP address to listen on
+		addr:    fmt.Sprintf(":%d", port),
+		players: make(map[*websocket.Conn]bool),
 	}
 }
 
@@ -24,7 +37,45 @@ func (s *Server) Start() error {
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/", fs)
 
+	// Configure websocket route.
+	http.HandleFunc("/ws", s.handleConnection)
+
 	// Start listening for incoming requests.
 	log.Printf("Starting server on %s...\n", s.addr)
 	return http.ListenAndServe(s.addr, nil)
+}
+
+func (s *Server) handleConnection(w http.ResponseWriter, r *http.Request) {
+	// Upgrade initial GET request to a websocket.
+	ws, err := s.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("Warning: ", err)
+		return
+	}
+	defer ws.Close()
+
+	// Register the new player.
+	s.players[ws] = true
+
+	// Display "@" character at top left of screen.
+	if err := ws.WriteJSON(Event{
+		Name: "display",
+		Data: `{"c": "@", "x": 0, "y": 0}`,
+	}); err != nil {
+		log.Print("Warning: ", err)
+		delete(s.players, ws)
+		return
+	}
+
+	for {
+		var e Event
+		// Wait for a new event to come in and parse it.
+		if err := ws.ReadJSON(&e); err != nil {
+			log.Print("Warning: ", err)
+			delete(s.players, ws)
+			break
+		}
+		// Just print the event for now.
+		log.Print(e)
+	}
 }
